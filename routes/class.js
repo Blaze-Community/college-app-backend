4bf8e7a14d8eb3b44d34f55d6495cc2d6000db12
Akash Deep <aakash20012408@gmail.com>
2021-06-22 13:36:57 +0530

    class

--- routes/class.js
+++ routes/class.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { createClass } = require('../controllers/class');
+const { createClass,joinClass,studentClasses,teacherClasses } = require('../controllers/class');
 const { validateLoginRequest, isRequestValidated, validateRegisterRequest } = require('../validators/auth');
 const router = express.Router();
 
 router.post("/createClass",isRequestValidated, createClass);
+router.post("/joinClass",isRequestValidated, joinClass);
+router.get("/studentClasses",isRequestValidated, studentClasses);
+router.get("/teacherClasses",isRequestValidated, teacherClasses);
 
 module.exports = router;
