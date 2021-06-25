cb809fd1c9163724a0b086171653f84eda3dcb09
Ashish Sonam <ashishsonam00@gmail.com>
2021-06-25 14:40:44 +0530

    Revert "token refresh"

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,34 +1,12 @@
-const jwt = require("jsonwebtoken");
-// const multer = require("multer");
-
-// const fileStorageEngine = multer.diskStorage({
-//   destination: (req, res, cb) => {
-//     cb(null, "./images");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + "_" + file.originalname);
-//   },
-// });
-
-// exports.upload = multer({ storage: fileStorageEngine });
+const jwt = require("jwt-simple");
+const secretKey = "secret";
 
 exports.requireSignin = (req, res, next) => {
+
     if (req.headers.authorization) {
         const token = req.headers.authorization.split(" ")[1];
-        jwt.verify(
-            token,
-            process.env.ACCESS_TOKEN_SECRET,
-            async (err, user) => {
-                if (user) {
-                    req.user = user;
-                } else if (err.message === "jwt expired") {
-                    return res.json({
-                        success: false,
-                        message: "Access token expired",
-                    });
-                }
-            }
-        );
+        const user = jwt.decode(token, secretKey);
+        req.user = user;
     } else {
         return res.status(400).json({ error: "Authorization required" });
     }
