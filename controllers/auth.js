43c501e908420b632aad5629379b79c5c14a1a9d
ashish <ashishsonam00@gmail.com>
2021-08-19 14:23:28 +0530

    reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,60 +2,66 @@ const express = require("express");
 const { collegeUser } = require("../models/user");
 const jwt = require("jsonwebtoken");
 const { hashPassword } = require("./hashPassword");
-
-// const ACCESS_TOKEN_SECRET = "thisIsAccessTokenSecretKey";
-// const REFRESH_TOKEN_SECRET = "thisIsRefreshTokenSecretKey";
+const nodemailer = require("nodemailer");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 
 exports.register = (req, res) => {
     const { user } = req.body;
     let newUser;
-    collegeUser.findOne({ email: user.email }).exec((error, existingUser) => {
-        if (error) {
-            return res.status(400).json({ error });
-        }
-        if (existingUser) {
-            return res.status(400).json({
-                error: "User with that email already exist",
-                existingUser: existingUser,
+    collegeUser
+        .findOne({ email: user.email })
+        .exec(async (error, existingUser) => {
+            if (error) {
+                return res.status(400).json({ error });
+            }
+            if (existingUser) {
+                return res.status(400).json({
+                    error: "User with that email already exist",
+                    existingUser: existingUser,
+                });
+            } else {
+                let token = crypto.randomBytes(32).toString("hex");
+                const hash = await hashPassword(token);
+                if (user.role === "student") {
+                    newUser = collegeUser({
+                        profileName: user.profileName,
+                        profilePhotoUri: user.profilePhotoUri,
+                        e_card: user.e_card,
+                        role: user.role,
+                        email: user.email,
+                        password: user.password,
+                        batch: user.batch,
+                        rollno: user.rollno,
+                        branch: user.branch,
+                        section: user.section,
+                        token: hash,
+                    });
+                } else {
+                    newUser = collegeUser({
+                        profileName: user.profileName,
+                        profilePhotoUri: user.profilePhotoUri,
+                        role: user.role,
+                        email: user.email,
+                        password: user.password,
+                        token: hash,
+                    });
+                }
+            }
+            newUser.save((err, newUser) => {
+                if (err) {
+                    res.status(400).json({
+                        success: false,
+                        msg: "Failed to save in Users",
+                    });
+                } else {
+                    res.status(200).json({
+                        success: true,
+                        msg: "Successfully saved",
+                    });
+                }
             });
-        } else {
-            if (user.role === "student") {
-                newUser = collegeUser({
-                    profileName: user.profileName,
-                    profilePhotoUri: user.profilePhotoUri,
-                    e_card: user.e_card,
-                    role: user.role,
-                    email: user.email,
-                    password: user.password,
-                    batch: user.batch,
-                    rollno: user.rollno,
-                    branch: user.branch,
-                    section: user.section,
-                });
-            } else {
-                newUser = collegeUser({
-                    profileName: user.profileName,
-                    profilePhotoUri: user.profilePhotoUri,
-                    role: user.role,
-                    email: user.email,
-                    password: user.password,
-                });
-            }
-        }
-        newUser.save((err, newUser) => {
-            if (err) {
-                res.status(400).json({
-                    success: false,
-                    msg: "Failed to save in Users",
-                });
-            } else {
-                res.status(200).json({
-                    success: true,
-                    msg: "Successfully saved",
-                });
-            }
         });
-    });
 };
 
 exports.login = (req, res) => {
@@ -226,6 +232,177 @@ exports.changePassword = (req, res) => {
                         }
                     }
                 );
+            }
+        }
+    );
+};
+
+exports.requestResetPassword = (req, res) => {
+    let _email = req.body.email;
+    console.log(req.body);
+    collegeUser.findOne(
+        {
+            email: _email,
+        },
+        async (err, userFound) => {
+            if (err) {
+                console.log(err);
+                return res
+                    .status(500)
+                    .json({ success: true, msg: err.message });
+            }
+            if (!userFound) {
+                res.status(400).send({
+                    success: false,
+                    msg: "User not found",
+                });
+            } else {
+                let newValues = {
+                    $set: {
+                        token: "",
+                    },
+                };
+
+                if (userFound.token) {
+                    collegeUser.updateOne(
+                        { email: _email },
+                        newValues,
+                        (err, res) => {
+                            if (err) {
+                                throw err;
+                            }
+                        }
+                    );
+                }
+                let token = crypto.randomBytes(32).toString("hex");
+                const hash = await hashPassword(token);
+
+                newValues = {
+                    $set: {
+                        token: hash,
+                    },
+                };
+
+                collegeUser.updateOne(
+                    { email: _email },
+                    newValues,
+                    (err, res) => {
+                        if (err) {
+                            throw err;
+                        }
+                    }
+                );
+                const id = userFound._id;
+                let link = `http://localhost:5000/api/resetPassword/${id}/${token}`;
+                let transporter = nodemailer.createTransport({
+                    service: "gmail",
+                    auth: {
+                        user: "blazecommunity001@gmail.com",
+                        pass: "Fuckme123@@@",
+                    },
+                });
+                let mailOptions = {
+                    from: "blazecommunity001@gmail.com",
+                    to: _email,
+                    subject: "Password Reset - IIITN",
+                    text: `Here's the link to reset your password - ${link}`,
+                };
+                transporter.sendMail(mailOptions, function (err, data) {
+                    if (err) {
+                        console.log("Error : " + err);
+                    } else {
+                        console.log("Email sent successfully");
+                    }
+                });
+                res.send({
+                    success: true,
+                    msg: "email sent successfully",
+                });
+            }
+        }
+    );
+};
+
+exports.resetPasswordForm = (req, res) => {
+    res.send(`<!DOCTYPE html>
+                <html lang="en">
+                    <head>
+                        <meta charset="UTF-8" />
+                        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                        <title>Document</title>
+                    </head>
+                    <body>
+                        <form
+                            action="http://localhost:5000/api/resetPassword/${req.params.id}/${req.params.token}"
+                            method="POST"
+                        >
+                            <input name="newPassword" type="text" placeholder="new password" />
+                            <input
+                                name="reenterNewPassword"
+                                type="text"
+                                placeholder="re-enter new password"
+                            />
+                            <button type="submit">submit</button>
+                        </form>
+                    </body>
+                </html>
+                `);
+};
+
+exports.resetPassword = (req, res) => {
+    collegeUser.findOne(
+        {
+            _id: req.params.id,
+        },
+        async (err, userFound) => {
+            if (err) {
+                console.log(err);
+                return res
+                    .status(500)
+                    .json({ success: false, msg: err.message });
+            }
+            if (!userFound) {
+                res.status(400).send({
+                    success: false,
+                    msg: "User not found",
+                });
+            } else {
+                console.log(req.params.token);
+                console.log(userFound.token);
+                const isValid = await bcrypt.compare(
+                    req.params.token,
+                    userFound.token
+                );
+                if (!isValid) {
+                    throw new Error("Invalid or expired password reset token");
+                }
+                if (req.body.reenterNewPassword != req.body.newPassword) {
+                    return res.status(400).json({
+                        success: false,
+                        msg: "re-entered password does not match",
+                    });
+                } else {
+                    const hashed = await hashPassword(req.body.newPassword);
+                    const newValues = {
+                        $set: {
+                            password: hashed,
+                        },
+                    };
+                    collegeUser.updateOne(
+                        { _id: req.params.id },
+                        newValues,
+                        (err, res) => {
+                            if (err) {
+                                throw err;
+                            }
+                        }
+                    );
+                    return res.send({
+                        success: true,
+                        msg: "password reset successful",
+                    });
+                }
             }
         }
     );
